e0f989c1345667bc71c7d28f4444b6e203cecb58
AngelFelipeMP <angel.magnossao@gmail.com>
2022-03-07 14:55:28 +0100

    "add Transformers + classfier to  model.py"

--- model.py
+++ model.py
@@ -1,42 +1,40 @@
 
-# import config
-# import transformers
-from tokenize import tokenize
+import torch
 import torch.nn as nn
-
-
-# class TransforomerModel(nn.Module):
-#     def __init__(self):
-#         super(TransforomerModel, self).__init__()
-#         self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH)
-#         self.bert_drop = nn.Dropout(0.3)
-#         self.out = nn.Linear(768, 1)
-
-#     def forward(self, ids, mask, token_type_ids):
-#         _, o2 = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
-#         bo = self.bert_drop(o2)
-#         output = self.out(bo)
-#         return output
-    
-
 from transformers import AutoModel, AutoTokenizer
 
-pre_trained_model = "bert-base-uncased"
-transformer = AutoModel.from_pretrained(pre_trained_model)
-tokenizer = AutoTokenizer.from_pretrained(pre_trained_model)
 
-max_len = 256
-Example1 = "Angel table home car"
-Example2 = "bhabha char roofing house get"
+class TransforomerModel(nn.Module):
+    def __init__(self, pre_trained_model=str(), drop_out=float(), embedding_size=int(), number_of_classes=int(), transformer_output=str()):
+        super(TransforomerModel, self).__init__()
+        self.number_of_classes = number_of_classes
+        self.embedding_size = embedding_size
+        self.transformer_output = transformer_output
+        self.transformer = AutoModel.from_pretrained(pre_trained_model, pooler_output=True)
+        self.dropout = nn.Dropout(drop_out)
+        self.classifier = classifier_()
+        
+        
+    def classifier_(self):
+        if self.transformer_output == 'last_hidden_state':
+            return nn.Linear(self.embedding_size * 2, self.number_of_classes)
+        elif self.transformer_output == 'pooler':
+            return nn.Linear(self.embedding_size, self.number_of_classes)
+        else:
+            print('The tramsformer output must be: /n (A) *last_hidden_state* or (B) *pooller* .')
+            print('Use one of the two options!')
+            exit()
+        
 
-pt_batch = tokenizer(
-    [Example1, Example2],
-    padding=True,
-    truncation=True,
-    add_special_tokens=True,
-    max_length=max_len,
-    return_tensors="pt")
-
-print(pt_batch)
-
-#TODO discover if have max token as a input is importat or I can send the frase with its own size
+    def forward(self, ids=int(), mask=int(), token_type_ids=int()):
+        if self.transformer_output == 'last_hidden_state':
+            last_hidden_state, _ = self.transformer(ids, attention_mask=mask, token_type_ids=token_type_ids)
+            mean_pool = torch.mean(last_hidden_state, 1)
+            max_pool = torch.max(last_hidden_state, 1)
+            cat = torch.cat((mean_pool,max_pool), 1)
+            drop = self.dropout(cat)
+        elif self.transformer_output == 'pooler':
+            _ , poller_output = self.transformer(ids, attention_mask=mask, token_type_ids=token_type_ids) 
+            drop = self.dropout(poller_output)
+    
+        return self.classifier(drop)
