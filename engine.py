3710d571eaa55744714ba63192f70f1e66a74e2f
AngelFelipeMP <angel.magnossao@gmail.com>
2022-03-08 12:16:12 +0100

    "create loss, train and val func"

--- engine.py
+++ engine.py
@@ -1,18 +1,75 @@
-pre_trained_model = "bert-base-uncased"
-transformer = AutoModel.from_pretrained(pre_trained_model)
-tokenizer = AutoTokenizer.from_pretrained(pre_trained_model)
+import torch
+import torch.nn as nn
+from tqdm import tqdm
 
-max_len = 15
-Example1 = "Angel table home car"
-Example2 = "bhabha char roofing house get"
-Example3 = "I wan to go to the beach for surfing"
 
-pt_batch = tokenizer(
-    [Example1, Example2, Example3],
-    padding=True,
-    truncation=True,
-    add_special_tokens=True,
-    max_length=max_len,
-    return_tensors="pt")
+def loss_fn(outputs, targets):
+    return nn.CrossEntropyLoss()(outputs, targets)
 
-print(pt_batch)
+
+def train_fn(data_loader, model, optimizer, device, scheduler, epoch):
+    model.train()
+    fin_targets = []
+    fin_predictions = []
+    total_loss = 0
+    
+    progress_bar = tqdm(data_loader, desc='Epoch {:1d}'.format(epoch))
+
+    for d in progress_bar:
+        ids = d["ids"]
+        token_type_ids = d["token_type_ids"]
+        mask = d["mask"]
+        targets = d["targets"]
+
+        ids = ids.to(device, dtype=torch.long)
+        token_type_ids = token_type_ids.to(device, dtype=torch.long)
+        mask = mask.to(device, dtype=torch.long)
+        targets = targets.to(device, dtype=torch.float)
+
+        optimizer.zero_grad()
+        outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+        loss = loss_fn(outputs, targets)
+        
+        total_loss += loss.cpu().detach().numpy().tolist()
+        _, predictions = torch.max(outputs, 1)
+        fin_targets.extend(targets.cpu().detach().numpy().tolist())
+        fin_predictions.extend(predictions.cpu().detach().numpy().tolist())
+        
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        
+        progress_bar.set_postfix({'training_loss': '{:.3f}'.format(loss.cpu().detach().numpy().tolist()/len(d))})
+        
+    return fin_predictions, fin_targets, total_loss/len(data_loader)
+        
+        
+
+
+def eval_fn(data_loader, model, device):
+    model.eval()
+    fin_targets = []
+    fin_predictions = []
+    total_loss = 0
+    
+    with torch.no_grad():
+        for d in tqdm(data_loader):
+            ids = d["ids"]
+            token_type_ids = d["token_type_ids"]
+            mask = d["mask"]
+            targets = d["targets"]
+
+            ids = ids.to(device, dtype=torch.long)
+            token_type_ids = token_type_ids.to(device, dtype=torch.long)
+            mask = mask.to(device, dtype=torch.long)
+            targets = targets.to(device, dtype=torch.float)
+
+            outputs = model(ids=ids, mask=mask, token_type_ids=token_type_ids)
+            loss = loss_fn(outputs, targets)
+            total_loss += loss.cpu().detach().numpy().tolist()
+            
+            fin_targets.extend(targets.cpu().detach().numpy().tolist())
+            _, predictions = torch.max(outputs, 1)
+            fin_predictions.extend(predictions.cpu().detach().numpy().tolist())
+    
+    return fin_predictions, fin_targets, total_loss/len(data_loader)
