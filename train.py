9a501345e2a6243d443cc8257289589ddc15551a
AngelFelipeMP <angel.magnossao@gmail.com>
2022-03-11 18:14:29 +0100

    "add code to save train/cross-validation results"

--- train.py
+++ train.py
@@ -12,29 +12,8 @@ from sklearn import metrics
 from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
 
-seed_val = 17
-random.seed(seed_val)
-np.random.seed(seed_val)
-torch.manual_seed(seed_val)
-torch.cuda.manual_seed_all(seed_val)
 
-dfx = pd.read_csv(config.DATASET_TRAIN, sep='\t', index_col=config.DATASET_INDEX).fillna("none")
-skf = StratifiedKFold(n_splits=config.SPLITS, random_state=seed_val)
-
-for task in config.LABELS:
-    for transformer, embedding_size in config.TRANSFORMERS.item():
-        best_f1 = 0
-        for max_len in config.MAX_LEN:
-            for batch_size in config.BATCH_SIZE:
-                for lr in config.LR:
-            
-    
-for train_index, val_index in skf.split(dfx[config.DATASET_TEXT_PROCESSED], dfx[task]):
-    df_train = dfx.loc[train_index]
-    df_val = dfx.loc[val_index]
-    
-
-    def run(df_train, df_val, max_len, task, transformer, batch_size, drop_out, embedding_size, number_of_classes, lr, best_f1):
+def run(df_train, df_val, max_len, task, transformer, batch_size, drop_out, embedding_size, number_of_classes, lr, best_f1, df_results):
         
         train_dataset = dataset.TransformerDataset(
             review=df_train[config.DATASET_TEXT_PROCESSED].values,
@@ -97,17 +76,80 @@ for train_index, val_index in skf.split(dfx[config.DATASET_TEXT_PROCESSED], dfx[
             f1_val = metrics.f1_score(targ_val, pred_val, average=macro)
             acc_val = metrics.accuracy(targ_val, pred_val)
             
+            df_results.append({'task':task,
+                                'epoch':epoch,
+                                'transformer':transformer,
+                                'max_len':max_len,
+                                'batch_size':batch_size,
+                                'lr':lr,
+                                'accuracy_train':acc_train,
+                                'f1-macro_train':f1_train
+                                'accuracy_val':acc_val,
+                                'f1-macro_val':f1_val,
+                                ignore_index=True
+            }
+            
             print(f"f1-macro_training = {:.3f}  accuracy_training = {:.3f}  loss_training = {:.3f}".format(f1_train, acc_train, loss_train))
             print(f"f1-macro_val = {:.3f}  accuracy_val = {:.3f}  loss_val = {:.3f}".format(f1_val, acc_val, loss_val))
             if f1_val > best_f1:
                 torch.save(model.state_dict(), f'{config.LOGS_PATH}/task[{task}]_transformer[{transformer}]_epoch[{epoch}]_maxlen[{max_len}]_batchsize[{batch_size}]_dropout[{drop_out}]_lr[{lr}].model')
                 best_f1 = f1_val
 
+if __name__ == "__main__":
+    seed_val = 17
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)
+
+    dfx = pd.read_csv(config.DATASET_TRAIN, sep='\t', index_col=config.DATASET_INDEX).fillna("none")
+    skf = StratifiedKFold(n_splits=config.SPLITS, random_state=seed_val)
+
+    df_results = pd.DataFrame(columns=['task',
+                                        'epoch',
+                                        'transformer',
+                                        'max_len',
+                                        'batch_size',
+                                        'lr',
+                                        'accuracy_train','
+                                        'f1-macro_train',
+                                        'accuracy_val',
+                                        'f1-macro_val']
+    )
 
 
-    #TODO add save metrics
-    #TODO testar tqdm
+    for task in config.LABELS:
+        for transformer, embedding_size in config.TRANSFORMERS.item():
+            best_f1 = 0
+            for max_len in config.MAX_LEN:
+                for batch_size in config.BATCH_SIZE:
+                    for lr in config.LR:
+                
+        
+                        for train_index, val_index in skf.split(dfx[config.DATASET_TEXT_PROCESSED], dfx[task]):
+                            df_train = dfx.loc[train_index]
+                            df_val = dfx.loc[val_index]
+                            
+                            
+                            run(df_train, 
+                                df_val, 
+                                max_len, 
+                                task, 
+                                transformer, 
+                                batch_size, 
+                                drop_out, 
+                                embedding_size, 
+                                number_of_classes, 
+                                lr, best_f1, 
+                                df_results)
+        
+
     
+
+    #TODO testar data.py
+    #TODO testar utils.py
+    #TODO testar tqdm
+    #TODO test code with one aranic transformer
     #TODO algument green search tring to improve the optimazer
     
     
