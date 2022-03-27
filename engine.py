21b6823b8c918f1af742249cf74b77e7c925778b
AngelFelipeMP <angel.magnossao@gmail.com>
2022-03-27 16:02:12 +0200

    "add validation func"

--- engine.py
+++ engine.py
@@ -61,6 +61,39 @@ def eval_fn(data_loader, model, device):
 
 
 
+def predict_fn(data_loader, model, device):
+    model.eval()
+    fin_targets = []
+    fin_predictions = []
+    total_loss = 0
+    
+    with torch.no_grad():
+        for batch in data_loader:
+            
+            batch = {k:v.to(device, dtype=torch.long) for k,v in batch.items()}
+            targets = batch["targets"]
+            del batch["targets"]
+
+            outputs = model(batch)
+            
+            fin_targets.extend(targets.cpu().detach().numpy().tolist())
+            fin_predictions.extend(outputs.cpu().detach().numpy().tolist())
+    
+    return fin_predictions, fin_targets
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # def loss_fn(outputs, targets):
 #     return nn.CrossEntropyLoss()(outputs, targets)
 
